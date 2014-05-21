1da7f853b97021ea6e473cdec075c3607adfe266
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-21 15:05:19 -0700

    adding user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(f_name: 'Guest', l_name: 'User', email: 'email@example.com', password: 'password')
