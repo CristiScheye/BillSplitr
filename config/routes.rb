d56d37f08665348aefc354138a6f55794f2c6701
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-21 18:54:15 -0700

    expose api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 BillSplit::Application.routes.draw do
-  resources :users
   resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:show] do
+      resources :bills, only: [:create]
+    end
+
+    resources :bills, except: [:index, :create] do
+      resources :bill_shares, only: [:index, :create]
+    end
+
+    resources :bill_shares, only: [:update, :destroy]
+  end
 
   root to: 'static_pages#home'
 end
