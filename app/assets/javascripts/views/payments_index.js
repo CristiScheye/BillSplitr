2317edf436ec750a435292dc9cef79d6bdd6254a
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-24 10:21:05 -0700

    add button to toggle new payment subview

--- app/assets/javascripts/views/payments_index.js
+++ app/assets/javascripts/views/payments_index.js
@@ -1,15 +1,15 @@
 window.BillSplit.Views.PaymentsIndex = Backbone.CompositeView.extend({
   initialize: function (options) {
     this.users = options.users;
-
     this.listenTo(this.collection, 'sync', this.render)
-
-    var newPayment = new BillSplit.Views.NewPayment({
-      collection: this.collection,
-      users: this.users
-    });
-    this.addSubview('#new-payment', newPayment)
   },
+
+  events: {
+    'click a#toggle-new-payment-form' : 'toggleNewPaymentForm'
+  },
+
+  template: JST['payments/index'],
+
   render: function() {
     var content = this.template({
       payments: this.collection
@@ -20,5 +20,25 @@ window.BillSplit.Views.PaymentsIndex = Backbone.CompositeView.extend({
 
     return this;
   },
-  template: JST['payments/index']
+
+  toggleNewPaymentForm: function (event) {
+    event.preventDefault();
+    var el = $(event.target);
+
+    if (el.hasClass('show-form')) {
+      el.html('[ - ] Record New Payment');
+
+      var newPayment = new BillSplit.Views.NewPayment({
+        collection: this.collection,
+        users: this.users
+      });
+      this.addSubview('#new-payment', newPayment)
+    } else {
+      el.html('[ + ] Record New Payment');
+
+      this.removeSubviews('#new-payment');
+    }
+
+    el.toggleClass('show-form')
+  }
 })
