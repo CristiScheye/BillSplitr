a6e035d1da637a78dd759265763e9c24dbc78eb7
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-27 17:29:16 -0700

    reformat new payment form

--- app/assets/javascripts/views/new_payment.js
+++ app/assets/javascripts/views/new_payment.js
@@ -1,14 +1,16 @@
 window.BillSplit.Views.NewPayment = Backbone.CompositeView.extend({
   initialize: function (options) {
-    this.listenTo(this.collection, 'invalid', this.errorMsg)
-
     this.users = options.users;
+    this.formType = 'from-user';
+
+    this.listenTo(this.collection, 'invalid', this.errorMsg)
     this.listenTo(this.users, 'sync', this.render)
   },
 
   events: {
     'submit #new-payment' : 'submitPayment',
-    'focus #payment-date' : 'displayCalendar'
+    'focus #payment-date' : 'displayCalendar',
+    'click ul#form-type' : 'toggleForm'
   },
 
   template: JST['payments/new'],
@@ -29,8 +31,22 @@ window.BillSplit.Views.NewPayment = Backbone.CompositeView.extend({
   submitPayment: function (event) {
     event.preventDefault();
     this.removeSubviews('#errors'); //clear old error messages
+    debugger;
 
     var paymentAttrs = $(event.target).serializeJSON()['payment'];
+    if (this.formType = 'from-user') {
+      paymentAttrs.sender_id = BillSplit.currentUser.id;
+      paymentAttrs.receiver_id = $(event.target)
+    }
     this.collection.create(paymentAttrs);
+  },
+
+  toggleForm: function (event) {
+    debugger;
+    $(event.currentTarget).find('li').toggleClass('active');
+    this.formType = $(event.target).attr('data-id');
+    var label = this.$el.find('label#user-select');
+    var labelText = (this.formType === 'from-user' ? 'To' : 'From')
+    label.html(labelText);
   }
 })
