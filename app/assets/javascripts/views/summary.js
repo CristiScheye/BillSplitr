4a5d66fe8b86bfaf19594d63fcbb44ddf91461fa
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-29 10:31:45 -0700

    modal displays on click of "full balance settled" and displays payment info" but does not commit transaction

--- app/assets/javascripts/views/summary.js
+++ app/assets/javascripts/views/summary.js
@@ -1,17 +1,12 @@
 window.BillSplit.Views.Summary = Backbone.CompositeView.extend({
   initialize: function (options) {
     this.listenTo(this.collection, 'sync', this.render)
+    BillSplit.users.fetch();
   },
   events: {
     'click button.edit-balance' : 'showModal',
-    'click .bills-index' : 'toggleBillHistory',
     'click .modal-dismiss' : 'closeModal',
-    'click button.small-modal-show' : 'temp'
-  },
-
-  temp: function (event) {
-    debugger;
-    $('#myWeeModal').modal('show');
+    'click .bills-index' : 'toggleBillHistory'
   },
 
   template: JST['layouts/summary'],
@@ -24,38 +19,41 @@ window.BillSplit.Views.Summary = Backbone.CompositeView.extend({
     });
     this.$el.html(content);
     this.attachSubviews();
-
-    debugger;
     return this;
   },
 
   showModal: function (event) {
+    $('#editModal').on('show.bs.modal', function (e) {
+      var btnData = $(event.target)
+      var senderId = btnData.attr('data-sender');
+      var receiverId = btnData.attr('data-receiver');
+      var reqType = btnData.attr('data-status');
+      var amt = Math.abs(parseFloat(btnData.attr('data-amt')));
+
+      if (senderId) {
+        receiverId = BillSplit.currentUser.id;
+      } else {
+        senderId = BillSplit.currentUser.id;
+      }
+
+      var sender = BillSplit.users.get(senderId);
+      var receiver = BillSplit.users.get(receiverId);
+      var bodyText = '';
+
+      $('#modal-sender-data').html(sender.escape('f_name') + ' ' + sender.escape('l_name'));
+      $('#modal-receiver-data').html(receiver.escape('f_name') + ' ' + receiver.escape('l_name'));
+      if (reqType === 'paid') {
+        $('#editModalLabel').html('Payment Confirmation')
+        bodyText += ' paid '
+      } else if (reqType === 'void'){
+        $('#editModalLabel').html('Void Confirmation')
+        bodyText += ' does NOT owe '
+      }
+      bodyText += accounting.formatMoney(amt) + ' to '
+      $('#modal-other-data').html(bodyText)
+    })
+
     $('#editModal').modal('show');
-    // var senderId = $(event.target).attr('data-sender');
-    // var receiverId = $(event.target).attr('data-receiver');
-    //
-    // if (senderId) {
-    //   receiverId = BillSplit.currentUser.id;
-    // } else {
-    //   senderId = BillSplit.currentUser.id;
-    // }
-    //
-    // var editModal = new BillSplit.Views.EditBalanceConfirmation({
-    //   senderId: senderId,
-    //   receiverId: receiverId,
-    //   balance: $(event.target).attr('data-amount'),
-    //   status: $(event.target).attr('data-status')
-    // })
-
-    // if user selects 'OK', call editBalance(event)
-    // else do nothing
-    // close modal
-  },
-
-  closeModal: function (event) {
-    debugger;
-    event.preventDefault();
-    alert('closing modal!');
   },
 
   editBalance: function (event) {
